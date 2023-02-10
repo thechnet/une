thechnet/une

commit 8d7f1d0048532c8bc527900e5d3562e1859cd2dc

Use une_run_bare in place of une_run in script() and eval()
This fixes an issue where errors would still be displayed even when
'cover'ing them.

src/builtin_functions.c
@@ -392,7 +392,7 @@ une_builtin_fn__(script)
   }
 
   /* Run script. */
-  une_result out = une_run(true, path, NULL, NULL, is);
+  une_result out = une_run_bare(error, is, NULL, args[script].value._wcs);
   free(path);
   return out;
 }
@@ -511,13 +511,12 @@ Evaluate a Une script in string form.
 */
 une_builtin_fn__(eval)
 {
-  une_builtin_param eval = 0;
+  une_builtin_param script = 0;
   
-  UNE_BUILTIN_VERIFY_ARG_TYPE(eval, UNE_RT_STR);
+  UNE_BUILTIN_VERIFY_ARG_TYPE(script, UNE_RT_STR);
 
   /* Run script. */
-  une_result out = une_run(false, NULL, args[eval].value._wcs, NULL, is);
-  return out;
+  return une_run_bare(error, is, NULL, args[script].value._wcs);
 }
 
 /*

commit 7f354ad244eb2be2d5b5ea8780a4d9d386f84710

Fix a bug introduced in 8d7f1d0

src/builtin_functions.c
@@ -1,6 +1,6 @@
 /*
 builtin.c - Une
-Modified 2023-02-09
+Modified 2023-02-10
 */
 
 /* Header-specific includes. */
@@ -392,7 +392,7 @@ une_builtin_fn__(script)
   }
 
   /* Run script. */
-  une_result out = une_run_bare(error, is, NULL, args[script].value._wcs);
+  une_result out = une_run_bare(error, is, path, NULL);
   free(path);
   return out;
 }